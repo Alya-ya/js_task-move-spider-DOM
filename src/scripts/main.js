83cdeb3c919327742de77941c7fba619777b0a15
Alya <alinataranik03@gmail.com>
2025-08-25 16:56:04 +0300

    add task solution 25.08.25

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,39 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
+const spider = document.body.querySelector('.spider');
+const wall = document.body.querySelector('.wall');
+
+const spiderSize = parseInt(getComputedStyle(spider).height, 10);
 
 document.addEventListener('click', (e) => {
-  const link = e.target.closest('.wall');
+  const coordX = e.clientX;
+  const coordY = e.clientY;
 
-  if (!link) {
-    return;
+  const wallBorder = parseInt(getComputedStyle(wall).borderWidth, 10);
+
+  const wallRect = wall.getBoundingClientRect();
+
+  if (e.target.closest('.wall')) {
+    let vertShift = coordY - wallRect.top - wallBorder - spiderSize / 2;
+    let horizShift = coordX - wallRect.left - wallBorder - spiderSize / 2;
+
+    if (vertShift < spiderSize) {
+      vertShift = 0;
+    }
+
+    if (wall.clientHeight - vertShift < spiderSize) {
+      vertShift = wall.clientHeight - spiderSize;
+    }
+
+    if (horizShift < spiderSize) {
+      horizShift = 0;
+    }
+
+    if (wall.clientWidth - horizShift < spiderSize) {
+      horizShift = wall.clientWidth - spiderSize;
+    }
+
+    spider.style.top = `${vertShift}px`;
+    spider.style.left = `${horizShift}px`;
   }
-
-  const x = e.pageX - wall.offsetLeft - wall.clientLeft;
-  const y = e.pageY - wall.offsetTop - wall.clientTop;
-  let leftPosition = x - spider.clientWidth / 2;
-  let topPosition = y - spider.clientHeight / 2;
-
-  if (leftPosition < 0) {
-    leftPosition = 0;
-  }
-
-  if (topPosition < 0) {
-    topPosition = 0;
-  }
-
-  if (leftPosition > wall.clientWidth - spider.clientWidth) {
-    leftPosition = wall.clientWidth - spider.clientWidth;
-  }
-
-  if (topPosition > wall.clientHeight - spider.clientHeight) {
-    topPosition = wall.clientHeight - spider.clientHeight;
-  }
-
-  spider.style.left = leftPosition + 'px';
-  spider.style.top = topPosition + 'px';
 });
